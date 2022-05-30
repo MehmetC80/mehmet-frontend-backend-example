497c5420d0ef4375c64330724f979c1af0f41372
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-05-30 08:34:24 +0200

    add new heroku url

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -3,54 +3,50 @@ import axios from 'axios';
 import { FaSpinner } from 'react-icons/fa';
 import FadeIn from 'react-fade-in';
 
-const url = 'https://ejt-livecoding-apisite-back.herokuapp.com';
+const url = 'https://mc-heroku-backend-deploy.herokuapp.com';
 
 export const PageBooks = () => {
-	const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
 
-	useEffect(() => {
-		setTimeout(() => {
-			(async () => {
-				setBooks((await axios.get(url)).data);
-			})();
-		}, 0);
-	}, []);
+  useEffect(() => {
+    setTimeout(() => {
+      (async () => {
+        setBooks((await axios.get(url)).data);
+      })();
+    }, 0);
+  }, []);
 
-	return (
-		<>
-			<h2>Books</h2>
-			<FadeIn>
-				{books.length === 0 ? (
-					<div>
-						<FaSpinner className="spinner" />
-					</div>
-				) : (
-					<>
-						<p>There are {books.length} books:</p>
+  return (
+    <>
+      <h2>Books</h2>
+      <FadeIn>
+        {books.length === 0 ? (
+          <div>
+            <FaSpinner className='spinner' />
+          </div>
+        ) : (
+          <>
+            <p>There are {books.length} books:</p>
 
-						<div className="books">
-							{books.map((book, index) => {
-								return (
-									<div key={index} className="book">
-										<img
-											src={`https://edwardtanguay.netlify.app/share/images/techBooks/${book.idCode}.jpg`}
-											alt="book"
-										/>
-										<div className="info">
-											<div className="title">
-												{book.title}
-											</div>
-											<div className="description">
-												{book.description}
-											</div>
-										</div>
-									</div>
-								);
-							})}
-						</div>
-					</>
-				)}
-			</FadeIn>
-		</>
-	);
+            <div className='books'>
+              {books.map((book, index) => {
+                return (
+                  <div key={index} className='book'>
+                    <img
+                      src={`https://edwardtanguay.netlify.app/share/images/techBooks/${book.idCode}.jpg`}
+                      alt='book'
+                    />
+                    <div className='info'>
+                      <div className='title'>{book.title}</div>
+                      <div className='description'>{book.description}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </FadeIn>
+    </>
+  );
 };
